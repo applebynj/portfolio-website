fc6258edd5b8c8fab69229427e7309bb048370f3
Noah Appleby <appleby.n@husky.neu.edu>
2017-09-09 21:52:30 -0400

    fix server bug always sending index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'))
 
 // Handles all routes so you do not get a not found error
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+    response.sendFile(path.resolve(__dirname, 'public'))
 })
 
 app.listen(port)
