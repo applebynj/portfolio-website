1424c6c4632a3e11aef4c75e94c7af216e4bfaa3
Noah Appleby <appleby.n@husky.neu.edu>
2017-10-11 18:19:47 -0400

    revert changes and add github page dependency

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 3000
 const app = express()
 
 // serve static assets normally
-app.use(express.static(__dirname + '/docs'))
+app.use(express.static(__dirname + '/public'))
 
 // Handles all routes so you do not get a not found error
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'docs/index.html'))
+    response.sendFile(path.resolve(__dirname, 'public/index.html'))
 })
 
 app.listen(port)
