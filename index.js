3e638e3898b153a2186690dccb5a85415d4c26a1
Noah Appleby <appleby.n@husky.neu.edu>
2017-10-11 01:30:59 -0400

    fix refresh and back button bug, server always serves index now

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'))
 
 // Handles all routes so you do not get a not found error
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'public'))
+    response.sendFile(path.resolve(__dirname, 'public/index.html'))
 })
 
 app.listen(port)
