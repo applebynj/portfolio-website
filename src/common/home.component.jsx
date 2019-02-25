9b0f42b8160cc7b4870e9a29630ccfad1c627c29
Noah Appleby <appleby.n@husky.neu.edu>
2019-02-24 18:12:36 -0800

    cleanup all project view for new visual style

--- src/common/home.component.jsx
+++ src/common/home.component.jsx
@@ -12,14 +12,14 @@ class Home extends Component {
             $('#home-projects').removeClass("all-cards");
             $('#project-cards').removeClass("all-cards");
             $('.project-card').removeClass("all-cards");
-            $('#featured-project').removeClass("all-cards");
-            $('#home-projects-label button').html("see all projects");
+            $('#featured-project-info').removeClass("all-cards");
+            $('#featured-project-header').html("ALL PROJECTS");
         } else {
             $('#home-projects').addClass("all-cards");
             $('#project-cards').addClass("all-cards");
             $('.project-card').addClass("all-cards");
-            $('#featured-project').addClass("all-cards");
-            $('#home-projects-label button').html("show featured project");
+            $('#featured-project-info').addClass("all-cards");
+            $('#featured-project-header').html("ALL PROJECTS");
         }
     };
 
@@ -61,7 +61,7 @@ class Home extends Component {
                                             I’m currently seeking internship opportunities for the summer, and will soon begin my search for a full-time gig; I graduate from Northeastern University in Spring 2020.
                                         </p>
                                         <p>
-                                            If you have any questions, comments, or cat photos to share, you can reach me via <a>email</a> or find me on <a>linkedin</a>. 
+                                            If you have any questions, comments, or cat photos to share, you can reach me via <a >email</a> or find me on <a href="https://www.linkedin.com/in/noahappleby">linkedin</a>. 
                                         </p>
                                     </div>
                                     <ul>
@@ -75,11 +75,13 @@ class Home extends Component {
                                         </div>
                                     </ul>
                                 </div>
-                                <div id="home-hello-next" className="border font-light">
-                                    <div className="arrow-down left"></div>
-                                    <div>SCROLL TO CONTINUE</div>
-                                    <div className="arrow-down right"></div>
-                                </div>
+                                <a href="#projects" className="font-light">
+                                    <div id="home-hello-next" className="border">
+                                            <div className="arrow-down left"></div>
+                                                SCROLL OR CLICK TO CONTINUE
+                                            <div className="arrow-down right"></div>
+                                    </div>
+                                </a>
                             </div>
                         </section>
                     </div>
@@ -94,12 +96,12 @@ class Home extends Component {
                                             <div id="featured-project-info">
                                                 <p>{projects[0].content[0].body}</p>
                                                 <ul>
-                                                    <li>
-                                                        <button type="button">read more →</button>
-                                                    </li>
                                                     <li><Link to={"/projects/" + projects[0].name}>
-                                                        <button type="button" className="cta" onClick={() => {{this.showAllCards()}}}>all projects →</button>
+                                                        <button type="button">read more →</button>
                                                     </Link></li>
+                                                    <li>
+                                                        <button type="button" className="cta" onClick={() => {{this.showAllCards()}}}>all projects →</button>
+                                                    </li>
                                                 </ul>
                                             </div>
                                         </div>
