21b7b8a0d01c59415a4e3376dff862c0d54f6996
Noah Appleby <appleby.n@husky.neu.edu>
2019-02-25 21:06:35 -0800

    add backlog link to banner

--- src/common/banner.component.jsx
+++ src/common/banner.component.jsx
@@ -6,8 +6,8 @@ class Banner extends Component {
     render(){
         return (
             <div id="text-banner">
-                <div className="container"><h4>This site is a work in progress.
-                </h4></div>
+                <div className="container border"><a href="https://trello.com/b/ePatTVuq/portfolio-site"><h4>This site is a work in progress, click here to see the backlog.
+                </h4></a></div>
             </div>
         );
     }
