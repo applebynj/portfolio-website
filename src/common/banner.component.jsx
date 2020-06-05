621b1813a741793b3af1655d42bdce45273c1db3
Noah Appleby <applebynj@gmail.com>
2020-06-05 11:09:23 -0700

    update banner

--- src/common/banner.component.jsx
+++ src/common/banner.component.jsx
@@ -6,7 +6,7 @@ class Banner extends Component {
     render(){
         return (
             <div id="text-banner">
-                <div className="container border"><a href="https://trello.com/b/ePatTVuq/portfolio-site"><h4>This site is a work in progress, click here to see the backlog.
+                <div className="container border"><a href="https://noahappleby.com"><h4>This is an archive of my old site. Click here to see the new one!
                 </h4></a></div>
             </div>
         );
